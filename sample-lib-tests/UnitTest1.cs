e0aec66e168e97e2177335833ce3c7e64ed44faa
Mirca Milanov <mikamilanov95@gmail.com>
2020-07-26 13:33:07 +0200

    Initial

---
+++ sample-lib-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace sample_lib_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ sample-lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace sample_lib
+{
+    public class Class1
+    {
+    }
+}
