b4493de3a8bb85217d80eca168ffb01593084195
Mirca Milanov <mikamilanov95@gmail.com>
2020-07-26 14:27:26 +0200

    Failed tests

--- sample-lib-tests/Class1Tests.cs
+++ sample-lib-tests/Class1Tests.cs
@@ -4,17 +4,27 @@ using sample_lib;
 
 namespace sample_lib_tests
 {
-    public class UnitTest1
+  public class Class1Tests
+  {
+    [Fact]
+    public void SayHello_ReturnHello()
     {
-        [Fact]
-        public void Test1()
-        {
-          //Arange
-          string expected = "Hello.";
-          //Act
-          string actual = Class1.SayHello();
-          //Assert
-          Assert.Equal(expected, actual);
-        }
+      //Arange
+      string expected = "Hello.";
+      //Act
+      string actual = Class1.SayHello();
+      //Assert
+      Assert.Equal(expected, actual);
     }
+    [Fact]
+    public void Sum_ReturnSum()
+    {
+      //Arange
+      int expected = 3;
+      //Act
+      int actual = Class1.Sum(1, 3);
+      //Assert
+      Assert.Equal(expected, actual);
+    }
+  }
 }
--- sample-lib/Class1.cs
+++ sample-lib/Class1.cs
@@ -4,6 +4,7 @@ namespace sample_lib
 {
     public class Class1
     {
-      public static string SayHello() => "Hello."
+      public static string SayHello() => "Hello.";
+      public static int Sum(int a, int b) => a + b;
     }
 }
